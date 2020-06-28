2cf95cc09c398c11abfc97fd2dc3e2ec5308c191
Chris Jennewein <christopher.jennewein@gmail.com>
2020-06-27 22:40:21 -0700

    feature: Create Swift Package

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.2
+import PackageDescription
+
+let package = Package(
+    name: "PokemomAPI",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v11),
+        .watchOS(.v2),
+        .tvOS(.v9),
+    ],
+    products: [
+        .library(name: "PokemomAPI",
+                 targets: ["PokemonAPI"]),
+    ],
+    targets: [
+        .target(name: "PokemonAPI",
+                path: "PokemonAPI",
+                exclude: ["Example", "Tests"]),
+        .testTarget(name: "PokemonAPITests",
+                    dependencies: ["PokemonAPI"],
+                    path: "Tests",
+                    exclude: ["Example"])
+    ]
+)
